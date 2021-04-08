598a47124f6caad5f145474528ed503a7defe7f2
kkimimaro <dannu2001@gmail.com>
2021-04-09 01:05:52 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,89 @@
+import React, { useEffect } from "react";
+import TodoList from "./Todo/TodoList";
+import Context from "./context";
+import Loader from "./Loader";
+import Modal from "./Modal/Modal";
+
+const AddTodo = React.lazy(
+  () =>
+    new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(import("./Todo/AddTodo"));
+      }, 2000);
+    })
+);
+
+function App() {
+  const [todos, setTodos] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos?_limit=5")
+      .then((response) => response.json())
+      .then((todos) => {
+        setTimeout(() => {
+          setTodos(todos);
+          setLoading(false);
+        }, 2000);
+      });
+  }, []);
+
+  function toggleTodo(id) {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    );
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  }
+
+  function addTodo(title) {
+    setTodos(
+      todos.concat([
+        {
+          title,
+          id: Date.now(),
+          completed: false,
+        },
+      ])
+    );
+  }
+
+  return (
+    <Context.Provider value={{ removeTodo }}>
+      <div className="wrapper fadeInDown">
+        <div className="formContent">
+          <img
+            type="image"
+            src="https://coursework.vschool.io/content/images/2015/08/todo_logo.gif"
+            id="icon"
+            alt="User Icon"
+            height="55"
+            width="133"
+          />{" "}
+          <br />
+          <Modal />
+          <React.Suspense
+            fallback={<p style={{ color: "#c9ad86" }}>Loading...</p>}
+          >
+            <AddTodo onCreate={addTodo} />
+          </React.Suspense>
+          {loading && <Loader />}
+          {todos.length ? (
+            <TodoList todos={todos} onToggle={toggleTodo} />
+          ) : loading ? null : (
+            <p>No Todos!</p>
+          )}
+        </div>
+      </div>
+    </Context.Provider>
+  );
+}
+
+export default App;
